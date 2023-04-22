eae8b6ada357fd39df54c1170120968c99ed08e3
Trey Hope <trey.a.hope@gmail.com>
2023-04-21 22:38:21 -0500

    Removed testFileName
    * Removed unnecessary testFileName value.

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -4,8 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart';
 
 class StorageService {
-  static const String testFileName = 'eicar-test-file.com';
-
   Future<String> uploadFile({required File file}) async {
     try {
       final Reference ref = FirebaseStorage.instance.ref().child(
